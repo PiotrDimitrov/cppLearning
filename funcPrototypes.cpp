71e9ccfbf94e33889931f3eb719500438a16a4a0
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 03:40:00 +0300

    function prototypes usage example

--- funcPrototypes.cpp
+++ funcPrototypes.cpp
@@ -2,27 +2,23 @@
 #include <iostream>
 
 //functions in c++ requires strict order in your code
+namespace prttps{
+    // Function Prototype
+    int sum(int a, int b);
 
-// Function Prototypes
-int sum(int a, int b);
-void message();
+    void message(){
+        std::cout << "1 + 1 = " << sum(1, 1) << std::endl;
+    }
 
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
 
 void funcPrototypes() {
     int x, y;
-    message();
+    prttps::message();
     std::cin >> x >> y;
-    int z = sum(x, y);
+    int z = prttps::sum(x, y);
     std::cout << z;
-
-}
-//these functions are invisible for main as they are found after main function block
-//but prototypes allow to use them in main
-
-int sum(int a, int b) {
-    return a + b;
-}
-
-void message(){
-    std::cout << "Enter 2 integers: \n";
 }
