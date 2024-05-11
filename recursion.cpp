3d7a2e9c5c866739fae012aa779250d2455aae79
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 03:33:20 +0300

    recursion example

--- recursion.cpp
+++ recursion.cpp
@@ -1,14 +1,17 @@
 #include "topics.h"
 #include <iostream>
 
-int factorial01(int n){
-    if (n == 1) {
-        return 1; //end condition
-    } else {
-        return n * factorial01(n-1);  // recursive call
+namespace rcn{
+    int factorial(int n){
+        if (n == 1) {
+            return 1; //end condition
+        } else {
+            return n * factorial(n-1);  // recursive call
+        }
     }
 }
 
+
 void recursion() {
-    std::cout << factorial01(7);
+    std::cout << rcn::factorial(7);
 }
