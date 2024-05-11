40b16d203c4274a6254a1f236c446570127a2fe0
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 13:50:43 +0300

    templates of data types

--- templates.cpp
+++ templates.cpp
@@ -1,21 +1,23 @@
 #include "topics.h"
 #include <iostream>
 
-/*
+namespace tmpl{
+
 template <typename T> //T is name of type
-T sum02 (T a, T b) {    //type can be any name
+T sum1(T a, T b) {    //type can be any name
   return a + b;
 }
-*/
 
 template <typename T1, typename T2> //T1 and T2 are names of types
-T1 sum02 (T1 a, T2 b) {
+T1 sum(T1 a, T2 b) {
     return a + b;
 }
+}
+
 
 void templates() {
-    std::cout << sum02(5, 10) << std::endl; //int
-    std::cout << sum02(1.5, 2.3) << std::endl; //double
-    std::cout << sum02('1', '2') << std::endl; //char
-    std::cout << sum02(1.5, 2) << std::endl; //double and int
+    std::cout << tmpl::sum(5, 10) << std::endl; //int
+    std::cout << tmpl::sum(1.5, 2.3) << std::endl; //double
+    std::cout << tmpl::sum('1', '2') << std::endl; //char
+    std::cout << tmpl::sum(1.5, 2) << std::endl; //double and int
 }
