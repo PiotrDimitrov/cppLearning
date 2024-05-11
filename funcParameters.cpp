308316e456aee4689e8f1236d3c04c32d6966eb5
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 13:26:03 +0300

    parameters input to func by value, ref, ptr

--- funcParameters.cpp
+++ funcParameters.cpp
@@ -1,29 +1,28 @@
 #include "topics.h"
 #include <iostream>
 
-int func1(int a){ // parameter by value
-    a = 2;
-    return 0;
-}
-
-int func2(int &a){ // parameter by reference
-    a = 3;
-    return 0;
-}
-
-int func3(int* a){ // parameter by pointer
-    *a = 4;
-    return 0;
+namespace fprmt{
+    int func1(int a){ // parameter by value
+        a = 2;
+        return 0;
+    }
+    int func2(int &a){ // parameter by reference
+        a = 3;
+        return 0;
+    }
+    int func3(int* a){ // parameter by pointer
+        *a = 4;
+        return 0;
+    }
 }
 
 void funcParameters() {
     int n = 1;
     std::cout << n << std::endl;
-    func1(n);
+    fprmt::func1(n);
     std::cout << n << std::endl; // 1 (n is not changed)
-    func2(n);
+    fprmt::func2(n);
     std::cout << n << std::endl; // 3
-    func3(&n);
+    fprmt::func3(&n);
     std::cout << n << std::endl; // 4
-
 }
