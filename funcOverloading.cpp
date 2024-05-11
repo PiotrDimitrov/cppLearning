8357d451cba4c417065de32c08e08c1d3ca85fd5
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 13:27:39 +0300

    function overloading

--- funcOverloading.cpp
+++ funcOverloading.cpp
@@ -1,20 +1,21 @@
 #include "topics.h"
 #include <iostream>
 
-int sum01(int a, int b){
-    return a + b;
+namespace ovrld{
+    int sum(int a, int b){
+        return a + b;
+    }
+    double sum(double a, double b){ // Overload on type
+        return a + b;
+    }
+    int sum(int a, int b, int c){ // Overload on number of arguments
+        return a + b + c;
+    }
 }
 
-double sum01(double a, double b){ // Overload on type
-    return a + b;
-}
-
-int sum01(int a, int b, int c){ // Overload on number of arguments
-    return a + b + c;
-}
-//function sum01 has 2 overloads
+//function sum01 has 3 overloads
 void funcOverloading() {
-    std::cout << sum01(1, 2) << std::endl;
-    std::cout << sum01(1.4, 2.5) << std::endl;
-    std::cout << sum01(1, 2, 3) << std::endl;
+    std::cout << ovrld::sum(1, 2) << std::endl;
+    std::cout << ovrld::sum(1.4, 2.5) << std::endl;
+    std::cout << ovrld::sum(1, 2, 3) << std::endl;
 }
