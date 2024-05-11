ed236b65ff85d58eea1bd1650a99d2b9580de05c
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 13:32:56 +0300

    array trasmision to func by value and by ref

--- arrayInFunc.cpp
+++ arrayInFunc.cpp
@@ -2,41 +2,44 @@
 #include <iostream>
 #include <vector>
 
-void print_array(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
+namespace arrfnc{
+    void print_array(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
 
-void print_vector(std::vector<int> vec){
-    for (int i = 0; i < vec.size(); i++){
-        std::cout << vec[i] << " ";
+    void print_vector(std::vector<int> vec){
+        for (int i = 0; i < vec.size(); i++){
+            std::cout << vec[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
+
+    void func1(int arr[]){
+        arr[0] = 10;
+    }
+
+    void func2(std::vector<int> vec){ //value of vec in parameter
+        vec[0] = 10;
+    }
+
+    void func3(std::vector<int> &vec){ //reference of vec in parameter
+        vec[0] = 10;
+    }
 }
 
-void func1(int arr[]){
-    arr[0] = 10;
-}
-
-void func2(std::vector<int> vec){ //value of vec in parameter
-    vec[0] = 10;
-}
-
-void func3(std::vector<int> &vec){ //reference of vec in parameter
-    vec[0] = 10;
-}
 
 void arrayInFunc() {
     int arr[] = {1, 2, 3, 4, 5};
-    print_array(arr, 5); //1 2 3 4 5
-    func1(arr);
-    print_array(arr, 5);//10 2 3 4 5 (arrays element can be accesed by func1 without adress)
+    arrfnc::print_array(arr, 5); //1 2 3 4 5
+    arrfnc::func1(arr);
+    arrfnc::print_array(arr, 5);//10 2 3 4 5 (arrays element can be accesed by func1 without adress)
     std::vector<int> vec = {1, 2, 3, 4, 5};
-    print_vector(vec); //1 2 3 4 5
-    func2(vec);
-    print_vector(vec); //1 2 3 4 5
-    func3(vec);
-    print_vector(vec); //10 2 3 4 5
+    arrfnc::print_vector(vec); //1 2 3 4 5
+    arrfnc::func2(vec);
+    arrfnc::print_vector(vec); //1 2 3 4 5
+    arrfnc::func3(vec);
+    arrfnc::print_vector(vec); //10 2 3 4 5
 }
