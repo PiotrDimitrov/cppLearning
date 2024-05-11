9adba6cb65974e87dee8732a112e005af0a02a62
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-11 13:20:52 +0300

    ASCII

--- ascii.cpp
+++ ascii.cpp
@@ -6,8 +6,8 @@ void ascii() {
     setlocale(LC_ALL, "ru");
     //ASCII codes
     for (int i = 0; i < 256; i++) {        // codes between 33 and 127
-        std::cout << i << "\t" << char(i) << std::endl;  //are used as default english
-        //keyboard symbols
-    } //codes after 127 are national and are set by setlocale()
+        std::cout << i << "\t" << char(i) << std::endl;  //are used as default english keyboard symbols
+    }
+    //codes after 127 are national and are set by setlocale()
     //codes before 33 are not used
 }
